1cebf8947d12e9839561d6655b52a66efbf6fd82
Hannah Thompson <thompson.h@gmail.com>
2017-04-07 13:50:07 +1000

    added carrierwave

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,7 +1,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -28,6 +28,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
+  version :thumb do
+    process resize_to_fill: [200,200]
+  end
 
   # Create different versions of your uploaded files:
   # version :thumb do
