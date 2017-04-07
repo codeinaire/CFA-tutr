6f78c51525ad39cbc159fa4ee477cc3c9ff2d2c0
John S <alucinare@gmail.com>
2017-04-07 13:35:00 +1000

    Initial profile design

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,8 @@
 
 ActiveRecord::Schema.define(version: 20170407005004) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "profiles", force: :cascade do |t|
     t.string   "name"
@@ -55,8 +53,6 @@ ActiveRecord::Schema.define(version: 20170407005004) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-
   add_foreign_key "profiles", "users"
   add_foreign_key "skills", "users"
-
 end
