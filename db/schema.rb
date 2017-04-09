93bf202e0913c0d6e8203e8c932dde7edc93611c
John S <alucinare@gmail.com>
2017-04-09 12:34:47 +1000

    Added profile input functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407052510) do
+ActiveRecord::Schema.define(version: 20170409015308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170407052510) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "avatar"
+    t.text     "quote"
     t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
   end
 
